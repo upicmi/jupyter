3f548bc410f4c326ff6f9c87687b43455619a3e6
Mehdi Nouri <mehdi93x@gmail.com>
2025-05-10 02:07:02 +0330

    new changes!

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -46,29 +46,21 @@ c.JupyterHub.cookie_secret_file = "/data/jupyterhub_cookie_secret"
 c.JupyterHub.db_url = "sqlite:////data/jupyterhub.sqlite"
 
 # Allow all signed-up users to login
-c.Authenticator.allow_all = True
-
 # Authenticate users with Native Authenticator
-c.JupyterHub.authenticator_class = "nativeauthenticator.NativeAuthenticator"
+c.JupyterHub.authenticator_class = 'nativeauthenticator.NativeAuthenticator'
 
 # Allow anyone to sign-up without approval
 c.NativeAuthenticator.open_signup = True
 c.NativeAuthenticator.manage_password = True
 c.NativeAuthenticator.auto_login = False
 c.NativeAuthenticator.allow_self_approval = True
-c.JupyterHub.load_roles = [
-    {
-        "name": "laravel-api",
-        "scopes": [
-            "admin:users",
-            "read:users",
-            "users",
-        ],
-        "api_tokens": [
-            "bad0ea22a5ba5921e1a84f37f5fa0d38448c71df78cf832f85852de24b985471"
-        ],
+
+c.JupyterHub.api_tokens = {
+    "bad0ea22a5ba5921e1a84f37f5fa0d38448c71df78cf832f85852de24b985471": {
+        "username": "admin",
+        "scopes": ["admin:users", "users", "read:users"]
     }
-]
+}
 
 c.JupyterHub.template_paths = ['/srv/jupyterhub/templates']
 # Add this AFTER c.JupyterHub.extra_static_paths
