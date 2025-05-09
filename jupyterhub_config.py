358e263bbe59a0c72f7e013d58fac6eacfc3a43b
Mehdi Nouri <mehdi93x@gmail.com>
2025-05-10 01:43:24 +0330

    new changes!

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -53,6 +53,12 @@ c.JupyterHub.authenticator_class = "nativeauthenticator.NativeAuthenticator"
 
 # Allow anyone to sign-up without approval
 c.NativeAuthenticator.open_signup = True
+c.NativeAuthenticator.manage_password = True
+c.NativeAuthenticator.auto_login = False
+c.NativeAuthenticator.allow_self_approval = True
+c.JupyterHub.api_tokens = {
+    "bad0ea22a5ba5921e1a84f37f5fa0d38448c71df78cf832f85852de24b985471": "admin-username"
+}
 
 c.JupyterHub.template_paths = ['/srv/jupyterhub/templates']
 # Add this AFTER c.JupyterHub.extra_static_paths
